11afb0c530ae68a52703dcf4b333a069244b4292
Erlend Tobiassen <erlend.tobiassen@gmail.com>
2020-01-11 17:38:03 +0100

    Fix unicode x-width and non 1-byte characters messing up columns.

--- components/sykl-formatting/src/lib.rs
+++ components/sykl-formatting/src/lib.rs
@@ -2,6 +2,7 @@ use std::borrow::Cow;
 use std::iter::Iterator;
 
 use bikeshare::types::JoinedStation;
+use unicode_width::*;
 
 /// This struct represents a row in the pretty printed output.
 #[derive(Clone)]
@@ -45,27 +46,77 @@ pub fn pretty_print_stations<'a>(
 
     // Iterate once to find the longest string for each column.
     for row in rows.clone() {
-        len_name = len_name.max(row.name.len());
-        len_docks = len_docks.max(row.docks.len());
-        len_bikes = len_bikes.max(row.bikes.len());
+        len_name = len_name.max(row.name.width());
+        len_docks = len_docks.max(row.docks.width());
+        len_bikes = len_bikes.max(row.bikes.width());
     }
 
     len_name += EXTRA_PADDING;
     len_docks += EXTRA_PADDING;
     len_bikes += EXTRA_PADDING;
 
+    let mut padding_name = String::new();
+    let mut padding_docks = String::new();
+    let mut padding_bikes = String::new();
+
     for row in rows {
+        padding_name = pad_into_buffer(padding_name, &row.name, len_name, ' ');
+        padding_docks = pad_into_buffer(padding_docks, &row.docks, len_docks, ' ');
+        padding_bikes = pad_into_buffer(padding_bikes, &row.bikes, len_bikes, ' ');
+
         writeln!(
             out,
-            "{name:len_name$}{docks:len_docks$}{bikes:len_bikes$}",
+            "{name}{padding_name}{docks}{padding_docks}{bikes}{padding_bikes}",
             name = row.name,
             docks = row.docks,
             bikes = row.bikes,
-            len_name = len_name,
-            len_docks = len_docks,
-            len_bikes = len_bikes,
+            padding_name = padding_name,
+            padding_docks = padding_docks,
+            padding_bikes = padding_bikes,
         )?;
     }
 
     Ok(())
+}
+
+/// # Panics
+/// Panics if pad is not a single width character
+pub fn pad(unicode: &str, width: usize, pad: char) -> String {
+    pad_into_buffer(String::new(), unicode, width, pad)
+}
+
+/// # Panics
+/// Panics if pad is not a single width character
+pub fn pad_into_buffer(mut buffer: String, unicode: &str, width: usize, pad: char) -> String {
+    assert_eq!(pad.width(), Some(1));
+
+    let padding_size = width.checked_sub(unicode.width()).unwrap_or(0);
+    buffer.truncate(0);
+    buffer.reserve(padding_size * pad.len_utf8());
+
+    for _ in 0..padding_size {
+        buffer.push(pad);
+    }
+
+    buffer
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pad_to_long() {
+        assert_eq!(pad("to_long_string", 3, ' '), "");
+    }
+
+    #[test]
+    fn test_pad() {
+        assert_eq!(pad("hello", 10, ' '), "     ");
+    }
+
+    #[test]
+    fn test_pad_double_width() {
+        assert_eq!(pad("🙌", 4, ' '), "  ");
+    }
 }
