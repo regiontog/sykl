1911e76183077b851fb6eddab186187d8de0ac62
regiontog <regiontog@users.noreply.github.com>
2020-01-12 16:58:50 +0000

    Deploying to gh-pages from  a381d72a3fba9d34eba3ca20051310946ff27689

--- sykl_rest/sidebar-items.js
+++ sykl_rest/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["InternalError",""]],"fn":[["main",""]],"struct":[["all_stations",""],["station",""]],"type":[["Stations",""]]});
+initSidebarItems({"enum":[["InternalError",""]],"fn":[["main",""],["update_status_sync",""]],"struct":[["all_stations",""],["station",""]],"type":[["Stations",""]]});
